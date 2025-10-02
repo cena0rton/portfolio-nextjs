06c55c4f8925ab3724f3ec474256fb196b851e17
cena0rton <cena0rton@users.noreply.github.com>
2025-10-02 21:02:18 +0530

    Update Career and Projects components, update package dependencies

--- src/app/components/Career.tsx
+++ src/app/components/Career.tsx
@@ -23,7 +23,7 @@ const Career = () => {
         }
     ]   
   return (
-    <motion.div
+    <motion.div id="career"
     initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6, delay: 0.1 }}
--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -55,7 +55,6 @@ const Projects = () => {
   const [selectedProject, setSelectedProject] = React.useState<typeof projects[0] | null>(null);
   
 
-
   return (
     <div id="projects">
     <div  className='relative border-t border-neutral-200 mt-6'>
@@ -231,13 +230,14 @@ const Projects = () => {
               >
                 <motion.h2 
                 whileHover={{ x: 10 }}
-                  className='text-lg font-medium'
+                  className='text-lg font-medium flex items-center justify-between'
                   initial={{ opacity: 0, x: -20 }}
                   animate={{ opacity: 1, x: 0 }}
                   transition={{ duration: 0.1 }}
                   layoutId={`title-${project.title}`}
                 >
                   {project.title}
+                  <div><svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={1}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-external-link"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" /><path d="M11 13l9 -9" /><path d="M15 4h5v5" /></svg></div>
                 </motion.h2>
                 
                 <motion.p 
